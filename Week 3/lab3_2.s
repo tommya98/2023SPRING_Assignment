9ea4877fd0ae1dc77587d8107cd1c9ba0e6e7b7c
tommya98 <tommya98@naver.com>
2023-03-17 11:33:11 +0900

    Add Week 3 source file

---
+++ Week 3/lab3_2.s
@@ -0,0 +1,29 @@
+sram_base equ 0x200000
+bb_alias equ 0x22000000
+      area labc_3,code
+      entry
+__main proc
+      export __main [weak]
+start
+      ldr r0,=bb_alias
+      ldr r4,=sram_base
+      ldr r3,cmask
+      ldr r1,cdata
+      and r1,r3
+      str r1,[r4]
+;
+      ldr r1,cdata
+      str r1,[r4]
+      mov r5,#0
+      str r5,[r0,#0x2c]
+;
+      ldr r6,=0xbabeface
+      b .
+;     ltorg
+cdata dcd 0x34567890
+cmask dcd 0xfffff7ff
+      endp
+      align
+      area lab3data,data
+sdata space 0x100
+      end
